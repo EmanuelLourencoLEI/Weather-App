70d107e25be4d0e479e39aae74c6a73912b0650d
EmanuelLourencoLEI <manubarata1000@gmail.com>
2022-08-24 02:37:35 +0100

    Weather App Commit
    TODO:
    -Better UI on CityListFragment
    -UI does not refresh automatically when user gives permission

--- app/src/main/java/com/emanuel/lourenco/weatherapp/CityListAdapter.kt
+++ app/src/main/java/com/emanuel/lourenco/weatherapp/CityListAdapter.kt
@@ -3,15 +3,25 @@ package com.emanuel.lourenco.weatherapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Button
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
-class CityListAdapter:
+class CityListAdapter :
     RecyclerView.Adapter<CityListAdapter.CityListViewHolder>() {
 
-    private val list = mutableListOf("Lisbon","Madrid","Paris","Berlin","Copenhagen","Rome","London","Dublin","Prague","Vienna")
+    private val cityList = listOf(
+        "Lisbon",
+        "Madrid",
+        "Paris",
+        "Berlin",
+        "Copenhagen",
+        "Rome",
+        "London",
+        "Dublin",
+        "Prague",
+        "Vienna"
+    )
 
     /**
      * Provides a reference for the views needed to display items in your list.
@@ -21,7 +31,7 @@ class CityListAdapter:
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return cityList.size
     }
 
     /**
@@ -32,8 +42,6 @@ class CityListAdapter:
             .from(parent.context)
             .inflate(R.layout.city_view, parent, false)
 
-        // Setup custom accessibility delegate to set the text read
-        layout.accessibilityDelegate = Accessibility
         return CityListViewHolder(layout)
     }
 
@@ -41,36 +49,16 @@ class CityListAdapter:
      * Replaces the content of an existing view with new data
      */
     override fun onBindViewHolder(holder: CityListViewHolder, position: Int) {
-        val item = list[position]
+        val item = cityList[position]
         holder.button.text = item
 
         // Assigns a [OnClickListener] to the button contained in the [ViewHolder]
         holder.button.setOnClickListener {
             // using the required arguments
-            val action = CityListFragmentDirections.actionCityListFragmentToCityDetailFragment(cityName = holder.button.text.toString())
+            val action =
+                CityListFragmentDirections.actionCityListFragmentToCityDetailFragment(cityName = holder.button.text.toString())
             holder.view.findNavController().navigate(action)
         }
     }
 
-    // Setup custom accessibility delegate to set the text read with
-    // an accessibility service
-    companion object Accessibility : View.AccessibilityDelegate() {
-        override fun onInitializeAccessibilityNodeInfo(
-            host: View?,
-            info: AccessibilityNodeInfo?
-        ) {
-            super.onInitializeAccessibilityNodeInfo(host, info)
-            // With `null` as the second argument to [AccessibilityAction], the
-            // accessibility service announces "double tap to activate".
-            // If a custom string is provided,
-            // it announces "double tap to <custom string>".
-            val customString = host?.context?.getString(R.string.app_name)
-            val customClick =
-                AccessibilityNodeInfo.AccessibilityAction(
-                    AccessibilityNodeInfo.ACTION_CLICK,
-                    customString
-                )
-            info?.addAction(customClick)
-        }
-    }
 }
--- app/src/main/java/com/emanuel/lourenco/weatherapp/MainActivity.kt
+++ app/src/main/java/com/emanuel/lourenco/weatherapp/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.emanuel.lourenco.weatherapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.emanuel.lourenco.weatherapp.databinding.ActivityMainBinding
+import com.google.android.gms.location.*
+import java.util.*
+
+var locality: String = ""
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var geoCoder: Geocoder
+    private lateinit var addressList: MutableList<Address>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +30,16 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         // Get the navigation host fragment from this Activity
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         // Instantiate the navController using the NavHostFragment
         navController = navHostFragment.navController
+        geoCoder = Geocoder(this, Locale.getDefault())
+
+        getLastLocation()
     }
 
     /**
@@ -28,5 +47,73 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                val location: Location? = task.result
+                if (location == null) {
+                    val locationRequest = LocationRequest.create().apply {
+                        interval = 100
+                        fastestInterval = 50
+                        priority = Priority.PRIORITY_HIGH_ACCURACY
+                        maxWaitTime = 100
+                    }
+
+                    fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                    fusedLocationClient.requestLocationUpdates(
+                        locationRequest, locationCallback,
+                        Looper.myLooper()
+                    )
+                } else {
+                    addressList = geoCoder.getFromLocation(location.latitude, location.longitude, 1)
+                    locality = addressList[0].locality
+                }
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val lastLocation: Location? = locationResult.lastLocation
+            if (lastLocation != null) {
+                addressList = geoCoder.getFromLocation(lastLocation.latitude, lastLocation.longitude, 1)
+            }
+            locality = addressList[0].locality
+        }
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            100
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLastLocation()
+            }
+        }
     }
 }
